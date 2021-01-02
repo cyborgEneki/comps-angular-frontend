e658e6f42c2bc00d539667b22786c9f920156daa
Eneki <joanreneki@gmail.com>
2021-01-02 23:38:58 +0300

    Add full edit initiative form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { MenuSidenavComponent } from './menu-sidenav/menu-sidenav.component';
@@ -22,7 +25,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
     AppComponent,
     MenuSidenavComponent,
     EditInitiativeComponent,
-    WelcomeComponent
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -36,11 +39,13 @@ import { WelcomeComponent } from './welcome/welcome.component';
     MatIconModule,
     MatListModule,
     AppRoutingModule,
-    MatSelectModule
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
 
 // Register component in corresponding module (this is not done by default) then register the custom module in the main module. That way, Angular will know it exists.
