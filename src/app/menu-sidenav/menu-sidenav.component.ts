8c08210b68b838012d746138abc0ee14588700af
Eneki <joanreneki@gmail.com>
2021-01-03 13:36:53 +0300

    Display initiative name when clicked

--- src/app/menu-sidenav/menu-sidenav.component.ts
+++ src/app/menu-sidenav/menu-sidenav.component.ts
@@ -17,7 +17,8 @@ const GET_INITIATIVES = gql`
         leadEmail
         startYear
         endYear
-        statement
+        statement,
+        goalTeam
       }
     }
   }
@@ -26,10 +27,24 @@ const GET_INITIATIVES = gql`
 @Component({
   selector: 'app-menu-sidenav',
   templateUrl: './menu-sidenav.component.html',
-  styleUrls: ['./menu-sidenav.component.scss']
+  styleUrls: ['./menu-sidenav.component.scss'],
 })
 export class MenuSidenavComponent {
   goalTeams = [{ test: 'e' }, { test: 'f' }, { test: 'g' }];
+
+  displayInitiativeData(initiative: Object) {
+    // Goal team should appear in dropdown
+    let initiativeGoalTeam = document.getElementById('goal-team');
+    console.log(initiative);
+    // initiativeGoalTeam.value = initiative.goalTeam;
+    // console.log(initiativeGoalTeam.value);
+    // Route should change to edit form
+    // Form should be populated with initiative data
+
+    // When saving, the value of goal team should be added to the data sent by the request
+    console.log('display data')
+  };
+
   deleteIndicator() {
     console.log('delete this');
   }
@@ -40,7 +55,7 @@ export class MenuSidenavComponent {
       map((result) => result.matches),
       shareReplay()
     );
-    
+
   public initiatives!: Observable<any>;
 
   constructor(
