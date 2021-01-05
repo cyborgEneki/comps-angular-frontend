bb012fc75fe0a4525fde7e9f9289f8d5cee5770c
Eneki <joanreneki@gmail.com>
2021-01-05 10:24:44 +0300

    Clear all dead code related to goal teams on the sidenav

--- src/app/menu-sidenav/menu-sidenav.component.ts
+++ src/app/menu-sidenav/menu-sidenav.component.ts
@@ -32,50 +32,12 @@ const GET_INITIATIVES = gql`
   }
 `;
 
-const GET_GOAL_TEAMS = gql`
-  {
-    goalTeams {
-      goalTeams {
-        _id
-        name
-      }
-    }
-  }
-`;
-
 @Component({
   selector: 'app-menu-sidenav',
   templateUrl: './menu-sidenav.component.html',
   styleUrls: ['./menu-sidenav.component.scss'],
 })
 export class MenuSidenavComponent {
-  goalTeamForm = new FormGroup({
-    goalTeam: new FormControl(''),
-  });
-
-  // stateFormControl = new FormControl('', [Validators.required]);
-
-  displayInitiativeData(initiative: any) {
-    this.goalTeamForm.controls['goalTeam'].setValue(initiative.goalTeam._id);
-    this.router.navigate(['/edit-initiative', initiative._id]);
-
-    // When saving, the value of goal team should be added to the data sent by the request
-    // Clear on reload
-    console.log('display data');
-  }
-
-  deleteIndicator() {
-    console.log('delete this');
-  }
-
-  public addGoalTeamToInitiative() {
-    const currentItem: EIItem = {
-      goalTeam: this.goalTeamForm.value
-    };
-
-    this.editInitiatiaveService.addData(currentItem);
-  }
-
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
@@ -84,13 +46,11 @@ export class MenuSidenavComponent {
     );
 
   initiatives!: Observable<any>;
-  goalTeams!: Observable<any>;
 
   constructor(
     private breakpointObserver: BreakpointObserver,
     private apollo: Apollo,
-    private router: Router,
-    private editInitiatiaveService: EditInitiatiaveService
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -103,15 +63,15 @@ export class MenuSidenavComponent {
           return result.data.initiatives.initiatives;
         })
       );
+  }
 
-    this.goalTeams = this.apollo
-      .watchQuery({
-        query: GET_GOAL_TEAMS,
-      })
-      .valueChanges.pipe(
-        map((result: any) => {
-          return result.data.goalTeams.goalTeams;
-        })
-      );
+  displayInitiativeData(initiative: any) {
+    this.router.navigate(['/edit-initiative', initiative._id]);
+    // Clear on reload
+    console.log('display data');
+  }
+
+  deleteIndicator() {
+    console.log('delete this');
   }
 }
